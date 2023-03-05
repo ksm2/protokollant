f78c482965a4ff40551d53a8175dbdf23a97391a
Konstantin Möllers <ksm.moellers@gmail.com>
2023-03-05 20:54:07 +0100

    feat: parse command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(value_enum)]
+    change: Change,
+}
+
+#[derive(ValueEnum, Debug, Copy, Clone)]
+enum Change {
+    Major,
+    Minor,
+    Patch,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
