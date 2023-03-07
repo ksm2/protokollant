e5f5668ad5df75fb963c5ddbb76bf9ba18639d46
Konstantin Möllers <ksm.moellers@gmail.com>
2023-03-07 20:10:07 +0100

    feat: add model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod model;
+
 use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
---
+++ src/model.rs
@@ -0,0 +1,39 @@
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct Changelog {
+    pub intro: String,
+    pub versions: Vec<Version>,
+    pub refs: Vec<Ref>,
+}
+
+impl Changelog {
+    pub fn new() -> Self {
+        Self {
+            intro: String::new(),
+            versions: Vec::new(),
+            refs: Vec::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct Version {
+    pub version: String,
+    pub date: Option<String>,
+    pub intro: String,
+    pub added: Vec<String>,
+    pub removed: Vec<String>,
+    pub fixed: Vec<String>,
+    pub changed: Vec<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct Ref {
+    pub anchor: String,
+    pub href: String,
+}
+
+impl Ref {
+    pub fn new(anchor: String, href: String) -> Self {
+        Self { anchor, href }
+    }
+}
